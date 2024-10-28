e7893848b8540803828fc09507e91aa5cbf1d5e1
harmonymwirigi <harmonymwirigi99@gmail.com>
2024-10-28 03:53:32 +0300

    ensure database is created

--- app/models.py
+++ app/models.py
@@ -97,7 +97,7 @@ class AdminUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(1000), nullable=False)
     phone = db.Column(db.String(20))
 
     def __repr__(self):
